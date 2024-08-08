f4b3a0874c5893026e70604cb680e98b118334bd
TynaiS <tynai.s.95@gmail.com>
2024-08-08 23:46:19 +0600

    first attempt to fix multiple registration issue

--- src/main/java/com/java_crud/auth/AuthenticationService.java
+++ src/main/java/com/java_crud/auth/AuthenticationService.java
@@ -19,19 +19,29 @@ public class AuthenticationService {
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
-    public AuthenticationResponse register(RegisterRequest request) {
-        var user = User.builder()
-                .firstname(request.getFirstName())
-                .lastname(request.getLastName())
-                .email(request.getEmail())
-                .password(passwordEncoder.encode(request.getPassword()))
-                .role(Role.USER)
-                .build();
-        repository.save(user);
-        var jwtToken = jwtService.generateToken(user);
-        return AuthenticationResponse.builder()
-                .token(jwtToken)
-                .build();
+    public AuthenticationResponse register(RegisterRequest request)
+        {
+        var userAlreadyRegistered = repository.findByEmail(request.getEmail()).isPresent();
+
+        if (!userAlreadyRegistered) {
+            var user = User.builder()
+                    .firstname(request.getFirstName())
+                    .lastname(request.getLastName())
+                    .email(request.getEmail())
+                    .password(passwordEncoder.encode(request.getPassword()))
+                    .role(Role.USER)
+                    .build();
+            repository.save(user);
+            var jwtToken = jwtService.generateToken(user);
+            return AuthenticationResponse.builder()
+                    .token(jwtToken)
+                    .build();
+        } else {
+            return AuthenticationResponse.builder()
+                    .message("This email already registered")
+                    .build();
+        }
+
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
